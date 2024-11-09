e97cb9e4dadac1874d323ee2831cb4e5f1dc1b25
Harsh1925 <d007modi@gmail.com>
2024-11-08 19:07:31 -0500

    new com2

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
-export const App = () => {
+const App = () => {
   return (
     <main className="max-w-7xl mx-auto">
       <h1 className="text-2xl text-white underline">Hello, Three.js!!</h1>
     </main>
   );
 };
+
+export default App;
