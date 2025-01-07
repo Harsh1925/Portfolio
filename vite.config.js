6eb0d314ca7ee88e6af2277f0a03888d2ae4d08e
Harsh1925 <d007modi@gmail.com>
2025-01-07 16:56:42 -0500

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,4 @@
-import {
-  vitePlugin as remix,
-  cloudflareDevProxyVitePlugin as remixCloudflareDevProxy,
-} from '@remix-run/dev';
+import { vitePlugin as remix, cloudflareDevProxyVitePlugin as remixCloudflareDevProxy } from '@remix-run/dev';
 import { defineConfig } from 'vite';
 import jsconfigPaths from 'vite-jsconfig-paths';
 import mdx from '@mdx-js/rollup';
@@ -28,7 +25,7 @@ export default defineConfig({
     remixCloudflareDevProxy(),
     remix({
       routes(defineRoutes) {
-        return defineRoutes(route => {
+        return defineRoutes((route) => {
           route('/', 'routes/home/route.js', { index: true });
         });
       },
